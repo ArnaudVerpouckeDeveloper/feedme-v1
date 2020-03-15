efc7c68d12bb15124548749bc46c3a75549772b1
OmedMilat <qais187@gmail.com>
2020-03-15 19:00:05 +0100

    feat: crud functions

--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -4,6 +4,7 @@ import Contact from '../views/Contact.vue'
 import Faq from '../views/Faq.vue'
 import Login from '../views/Login.vue'
 import Register from '../views/Register.vue'
+import Products from '../views/Products.vue'
 
 export const routes = [
     { path: '/', name: 'home', component: Home, display:'Home', icon: "mdi-home"},
@@ -12,4 +13,5 @@ export const routes = [
     { path: '/faq', name: 'faq', component: Faq, display:'Faq', icon: "mdi-email",},
     { path: '/login', name: 'login', component: Login, display:'Aanmelden', icon: "mdi-login-variant",},
     { path: '/register', name: 'register', component: Register, display:'Registreren', icon: "mdi-account-plus",},
+    { path: '/products', name: 'products', component: Products, display:'Producten', icon: "mdi-store",},
 ]
