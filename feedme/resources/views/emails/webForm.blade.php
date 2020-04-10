1315378b262436139853689c31e698f161c2193a
Arnaud Verpoucke <iemand4@gmail.com>
2020-04-10 13:25:52 +0200

    Adjusted mail layout

--- feedme/resources/views/emails/webForm.blade.php
+++ feedme/resources/views/emails/webForm.blade.php
@@ -4,9 +4,9 @@
   <title>Nieuw contactformulier</title>
 </head>
 <body style="font-family:Poppins,sans-serif;padding:2rem;">
-<p style="margin-bottom: 0.5rem; margin-top:0; font-weight: bold; width: 10rem;">Volledige naam: <span style="font-weight:normal;">{{$validatedData["fullName"]}}</span></p>
-<p style="margin-bottom: 0.5rem; margin-top:0; font-weight: bold; width: 10rem;">Emailadres:: <span style="font-weight:normal;">{{$validatedData["email"]}}</span></p>
-<p style="margin-bottom: 0.5rem; margin-top:0; font-weight: bold; width: 10rem;">Bericht:</p>
+<p style="margin-bottom: 0.5rem; margin-top:0; font-weight: bold;">Volledige naam: <span style="font-weight:normal;">{{$validatedData["fullName"]}}</span></p>
+<p style="margin-bottom: 0.5rem; margin-top:0; font-weight: bold;">Emailadres: <span style="font-weight:normal;">{{$validatedData["email"]}}</span></p>
+<p style="margin-bottom: 0.5rem; margin-top:0; font-weight: bold;">Bericht:</p>
 <p>{{$validatedData["message"]}}</p>
 
 </body>
