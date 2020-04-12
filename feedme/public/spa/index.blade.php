044b48c85bfe4107775b06a9c0f9e2e1f4eb2d3b
Arnaud Verpoucke <iemand4@gmail.com>
2020-04-12 14:21:00 +0200

    built the project

--- feedme/public/spa/index.blade.php
+++ feedme/public/spa/index.blade.php
@@ -8,19 +8,19 @@
     <title>Speed Meal</title>
     <link rel=stylesheet href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900">
     <link rel=stylesheet href=https://cdn.jsdelivr.net/npm/@mdi/font@latest/css/materialdesignicons.min.css>
-    <link rel=icon type=image/png href=/spa/assets/images/favicon.png>
-    <link href=/spa/css/app.52834cf5.css rel=preload as=style>
-    <link href=/spa/css/chunk-vendors.04c04cc1.css rel=preload as=style>
-    <link href=/spa/js/app.270ec4b6.js rel=preload as=script>
-    <link href=/spa/js/chunk-vendors.468927be.js rel=preload as=script>
-    <link href=/spa/css/chunk-vendors.04c04cc1.css rel=stylesheet>
-    <link href=/spa/css/app.52834cf5.css rel=stylesheet>
+    <link rel=icon type=image/png href=./spa/assets/images/favicon.png>
+    <link href=./spa/css/app.4ed7d8a6.css rel=preload as=style>
+    <link href=./spa/css/chunk-vendors.04c04cc1.css rel=preload as=style>
+    <link href=./spa/js/app.249d3f55.js rel=preload as=script>
+    <link href=./spa/js/chunk-vendors.468927be.js rel=preload as=script>
+    <link href=./spa/css/chunk-vendors.04c04cc1.css rel=stylesheet>
+    <link href=./spa/css/app.4ed7d8a6.css rel=stylesheet>
 </head>
 
 <body><noscript><strong>We're sorry but frontend doesn't work properly without JavaScript enabled. Please enable it to continue.</strong></noscript>
     <div id=app></div>
-    <script src=/spa/js/chunk-vendors.468927be.js></script>
-    <script src=/spa/js/app.270ec4b6.js></script>
+    <script src=./spa/js/chunk-vendors.468927be.js></script>
+    <script src=./spa/js/app.249d3f55.js></script>
 </body>
 
 </html>
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -10,5 +10,5 @@ const path = require("path");
 
 module.exports = {
     outputDir: path.resolve(__dirname, "../feedme/public/spa"),
-    //assetsDir: "./"
+    assetsDir: "./"
 }
