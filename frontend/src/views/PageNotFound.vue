b6f73836e8a1ad2d941c9a02cce29a59989e1704
Milatomed <milat.omed@studiohyperdrive.be>
2020-04-25 19:20:35 +0200

    feat: 404 page.

---
+++ frontend/src/views/PageNotFound.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container fluid fill-height>
+    <v-layout align-center justify-center style="text-align:center">
+      <v-flex >
+        <h1 class="display-2 primary--text">Whoops, 404</h1>
+
+        <p>De pagina die u zoekt, kon niet gevonden worden.</p>
+
+        <v-btn
+          :to="`/`"
+          color="primary"
+          outlined
+        >Haal me hier weg!</v-btn>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+
+</style>
